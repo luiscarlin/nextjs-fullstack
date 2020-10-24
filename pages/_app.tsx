7742b917b5ba6b33cda9cfe3ba2b2af956e7239f
Luis Carlin <luiscarlin@gmail.com>
2020-10-24 15:49:35 -0400

    add vscode settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/global..scss';
 
-// This default export is required in a new `pages/_app.js` file.
-export default function MyApp({ Component, pageProps }) {
+const App = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
-}
+};
+
+export default App;
